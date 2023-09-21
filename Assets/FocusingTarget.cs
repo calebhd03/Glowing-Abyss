53bd890a85a2854b636af75ea6755c0e875992ae
calebhd03 <calebhd03@gmail.com>
2023-09-20 23:21:36 -0400

    Added plankton follow target

--- Assets/FocusingTarget.cs
+++ Assets/FocusingTarget.cs
@@ -6,20 +6,50 @@ using UnityEngine;
 public class FocusingTarget : MonoBehaviour
 {
     [SerializeField] float pullingStrength = 1.0f;
-    List<PlanktonTracking> planktonTrackingsList= new List<PlanktonTracking>();
+    [SerializeField] List<PlanktonTracking> planktonTrackingsList= new List<PlanktonTracking>();
 
+    CircleCollider2D playerCol;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCol = GetComponent<CircleCollider2D>();
+
+        //Find plankton already sitting in circle
+        Collider2D[] ret = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y), playerCol.radius);
+        foreach(Collider2D c in ret)
+        {
+            PlanktonTracking pt = c.gameObject.GetComponent<PlanktonTracking>();
+            if (pt != null)
+            {
+                AddPlanktonToList(pt);
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         foreach(PlanktonTracking p in planktonTrackingsList)
         {
             p.MoveTowards(transform.position, pullingStrength);
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlanktonTracking pt = collision.gameObject.GetComponent<PlanktonTracking>();
+
+        if (pt == null)
+            return;
+        if (planktonTrackingsList.Contains(pt))
+            return;
+
+        AddPlanktonToList(pt);
+    }
+
+    public void AddPlanktonToList(PlanktonTracking pt)
+    {
+        Debug.Log("Added : " + pt.name + " to list");
+        planktonTrackingsList.Add(pt);
     }
 }
