8d01f1c1639184e9d645fd76eb6b25c5f5fa31fa
calebhd03 <calebhd03@gmail.com>
2023-09-21 00:10:02 -0400

    player follows finger

--- Assets/TrackTowardsFinger.cs
+++ Assets/TrackTowardsFinger.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
@@ -5,17 +6,22 @@ using UnityEngine.InputSystem;
 public class TrackTowardsFinger : MonoBehaviour
 {
     NavMeshAgent navMeshAgent;
+    private Camera cam;
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent= GetComponent<NavMeshAgent>();
+        cam = Camera.main;
     }
 
     public void OnTouchPosition(InputValue value)
     {
         Debug.Log("Value  = " + value);
-        Debug.Log("value.Get<Vector2>()  = " + value.Get<Vector2>());
-        navMeshAgent.destination = value.Get<Vector2>();
+        Debug.Log("value.Get<Vector2>()  = " + value.Get<Vector2>()); 
+        Vector2 pos = value.Get<Vector2>();
+        Vector3 dest = cam.ScreenToWorldPoint(new Vector3(pos.x, pos.y, cam.nearClipPlane));
+        Debug.Log("dest  = " + dest);
+        navMeshAgent.destination = dest;
     }
 
     // Update is called once per frame
