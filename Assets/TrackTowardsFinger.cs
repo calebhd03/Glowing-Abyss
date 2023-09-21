813bdc6e54a750dd5b6fb3c88165e18a9ee936ea
calebhd03 <calebhd03@gmail.com>
2023-09-21 00:02:38 -0400

    Fixed upside down character

--- Assets/TrackTowardsFinger.cs
+++ Assets/TrackTowardsFinger.cs
@@ -1,15 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class TrackTowardsFinger : MonoBehaviour
 {
+    NavMeshAgent navMeshAgent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //navMeshAgent= GetComponent<NavMeshAgent>();
     }
 
+    /*public void OnTouchPosition(InputValue value)
+    {
+        Debug.Log("Value  = " + value);
+        Debug.Log("value.Get<Vector2>()  = " + value.Get<Vector2>());
+        navMeshAgent.destination = value.Get<Vector2>();
+    }
+*/
     // Update is called once per frame
     void Update()
     {
