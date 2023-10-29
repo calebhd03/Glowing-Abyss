82b442f77eb0ac9e84165b06302d70117c2c7b37
calebhd03 <calebhd03@gmail.com>
2023-10-29 12:48:12 -0400

    added geyser sfx

--- Assets/Geyser.cs
+++ Assets/Geyser.cs
@@ -7,27 +7,57 @@ public class Geyser : MonoBehaviour
     [Header("Numbers")]
     public float sprayTime;
     public float cooldownTime;
+    public float audioFadeTime;
 
     [Header("Refrences")]
     public ParticleSystem bubbles;
 
+    AudioSource geyserSound;
+
     private void Start()
     {
+        geyserSound = GetComponent<AudioSource>();
         StartBubbles();
     }
 
     void StartBubbles()
     {
+        StartCoroutine(StartBubbleSound());
         bubbles.Play();
         StartCoroutine(BubblesCooldown());
     }
 
+    void StopBubbles()
+    {
+        StartCoroutine(StopBubbleSound());
+        bubbles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+    }
+
+    private IEnumerator StartBubbleSound()
+    {
+        float timeElapsed = 0f;
+        while (timeElapsed < audioFadeTime)
+        {
+            geyserSound.volume = Mathf.Lerp(0, 1, timeElapsed / audioFadeTime);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+    private IEnumerator StopBubbleSound()
+    {
+        float timeElapsed = 0f;
+        while (timeElapsed < audioFadeTime)
+        {
+            geyserSound.volume = Mathf.Lerp(1, 0, timeElapsed / audioFadeTime);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+
     private IEnumerator BubblesCooldown()
     {
-        Debug.Log("Start");
         yield return new WaitForSeconds(sprayTime);
-        Debug.Log("Pause");
-        bubbles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        StopBubbles();
         yield return new WaitForSeconds(cooldownTime);
         StartBubbles();
     }
